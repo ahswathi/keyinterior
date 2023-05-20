e05a211b8d318955da68281a08d5dc214b40d0ad
swathi <swathiashok94@gmail.com>
2023-05-20 09:04:08 +0530

    validation

--- src/Components/Header copy.js
+++ src/Components/Header copy.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
+import Popup from './Popup';
 import '../Styles/Header.css'
 
-const Header = () => {
+const Header = () => { 
+    const [isPopupOpen, setPopupOpen] = useState(false);
+
+    const togglePopup = () => {
+      setPopupOpen(!isPopupOpen);
+    };
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const handleDropdownToggle = () => {
+      setDropdownOpen(!isDropdownOpen);
+    };
   return (
-    <div className='lg:px-[80px] md:px-[80px] sm:px-[40px] px-[40px] bg-[#060606] '> 
-        <div className='flex justify-between py-[40px] '>
-        <div className='logo'>
-            <img src="logo.png" alt="logo" />
+    <div className='lg:px-[80px] md:px-[80px] sm:px-[40px] px-[20px] bg-[#060606] '> 
+        <div className='flex justify-between py-[20px]  lg:py-[40px] md:py-[40px] sm:py-[30px]'>
+        <div className='logo h-[45px] lg:h-[90px]  md:h-[90px]  sm:h-[45px]'>
+        <a href="/">
+         <img src="logo.png" alt="logo" className='h-[45px] lg:h-[90px] md:h-[90px]  sm:h-[45px]'/>
+         </a>
         </div>
-        <div className='nav-menu ' >
-            <ul className='lg:flex sm:flex hidden  nav'>
+        <div className='nav-menu flex  lg:py-[30px] md:py-[30px]  ' >
+            <ul className='font-poppins lg:flex lg:block md:flex md:block hidden nav lg:text-[16px] md:text-[10px] text-[12px]'>
                 <li>
                     <Link to={'/'}>Home</Link>
                 </li>
@@ -20,19 +33,48 @@ const Header = () => {
                 <li>
                     <Link to={'/services'}>Services</Link>
                 </li>
+                {/* <li  to={'/services'} className="relative">
+                    <button
+                        onClick={handleDropdownToggle}
+                        className="focus:outline-none text-white"
+                    >
+                        Services 
+                    </button>
+                    <ul
+                        className={`absolute mt-2  ${isDropdownOpen ? 'block' : 'hidden'}`}
+                    >
+                        <li>
+                        <Link to="/construction">Construction </Link>
+                        </li>
+                        <li>
+                        <Link to="/interior">Interior </Link>
+                        </li>
+                    </ul>
+                </li> */}
                 <li>
                     <Link to={'/contact'}>Contact</Link>
                 </li>
-            <button className='text-[#000000] font-bold	text-[14px] px-8 py-2  bg-[#FFC12B] '>Enquire now</button>
-
+            {/* <button className='text-[#000000] font-bold	text-[14px] px-8 py-2  bg-[#FFC12B] 
+            hover:bg-transparent hover:text-white hover:border-[#FFC12B] hover:border transition-colors duration-300'>
+            Enquire now</button> */}
+            <Popup />
             </ul>
-            <button className='block md:hidden sm:hidden lg:hidden py-3 px-4 mx-2 rounded '>
+            <button  className='block md:hidden sm:block lg:hidden py-3 px-4 mx-2 rounded group'>
                 <div className='w-6 h-1 bg-gray-500 mb-1'></div>
                 <div className='w-6 h-1 bg-gray-500 mb-1'></div>
                 <div className='w-6 h-1 bg-gray-500 mb-1'></div>
-                <div className='absolute top-0 -right-full transform group-focus:right-0 
-                transition-all duration-300'>
-                    <ul className='flex flex-col w-ful  pt-10 text-base'>
+              {/*   <div className='absolute ham-bar top-0 -right-full h-screen w-8/12 bg-white border
+                opacity-0 group-focus:right-0  group-focus:opacity-100 transition-all duration-300'>  */}
+                  <div className='absolute ham-bar top-0  h-screen bg-white border overflow-hidden
+                opacity-0 group-focus:right-0  group-focus:opacity-100 transition-all duration-300'> 
+                <div className="text-right"> 
+                <button
+                    onClick={togglePopup}
+                    className="mt-4  text-black font-bold py-2 px-4 rounded pr-[40px]"
+                    > X
+                </button>
+            </div>
+                    <ul className='flex flex-col w-ful  pt-[200px] text-center px-auto'>
                     <li>
                         <Link to={'/'}>Home</Link>
                     </li>
@@ -45,6 +87,7 @@ const Header = () => {
                     <li>
                         <Link to={'/contact'}>Contact</Link>
                     </li>
+                    {/* <Popup /> */}
                     </ul>
                 </div>
             </button>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -63,8 +63,10 @@ const Header = () => {
                 <div className='w-6 h-1 bg-gray-500 mb-1'></div>
                 <div className='w-6 h-1 bg-gray-500 mb-1'></div>
                 <div className='w-6 h-1 bg-gray-500 mb-1'></div>
-                <div className='absolute ham-bar top-0 -right-full h-screen w-8/12 bg-white border
-                opacity-0 group-focus:right-0  group-focus:opacity-100 transition-all duration-300'> 
+              {/*   <div className='absolute ham-bar top-0 -right-full h-screen w-8/12 bg-white border
+                opacity-0 group-focus:right-0  group-focus:opacity-100 transition-all duration-300'>  */}
+                  <div className='absolute ham-bar top-0  h-screen  bg-white border
+ opacity-1000 group-focus:right-0  group-focus:opacity-100 transition-all duration-600'> 
                 <div className="text-right"> 
                 <button
                     onClick={togglePopup}
@@ -72,17 +74,17 @@ const Header = () => {
                     > X
                 </button>
             </div>
-                    <ul className='flex flex-col w-ful  pt-[200px] text-center px-auto'>
-                    <li>
+                    <ul className='flex flex-col w-ful  pt-[200px] text-center '>
+                    <li className='ps-[30%]'>
                         <Link to={'/'}>Home</Link>
                     </li>
-                    <li>
+                    <li className='ps-[30%]'>
                         <Link to={'/about'}>About Us</Link>
                     </li>
-                    <li>
+                    <li className='ps-[30%]'>
                         <Link to={'/services'}>Services</Link>
                     </li>
-                    <li>
+                    <li className='ps-[30%]'>
                         <Link to={'/contact'}>Contact</Link>
                     </li>
                     {/* <Popup /> */}
