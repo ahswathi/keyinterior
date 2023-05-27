d6161b08cd1bea2f928c912b1e7cfb608a5ac53f
swathi <swathiashok94@gmail.com>
2023-05-27 12:27:16 +0530

    validation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,13 +17,13 @@ const Home = () => {
         text-[26px] px-[20px] py-[20px]">
           Transform Your Home Into<br/> A Haven With Our Interior <br/>Design Solutions
         </p>
-        <p className="lg:text-[58px] md:text-[58px] sm:text-[48px] hidden  lg:block md:block font-normal text-white lg:p-[80px] md:p-[80px] sm:pt-[20px]  sm:px-[80px] 
+        <p className="lg:text-[58px] md:text-[58px] sm:text-[48px] hidden  lg:block md:block font-normal text-white lg:p-[80px] md:p-[40px] sm:pt-[20px]  sm:px-[80px] 
         text-[28px] px-[20px] py-[20px]">
           Transform Your Home into a Haven with <br/>Our Interior Design Solutions
         </p>
         {/* <img src="images/index1.png" alt="logo" className='lg:px-[80px] lg:py-[80px] 
                                                           px-[40px] py-[20px]' /> */}
-          <div  className='lg:px-[80px] lg:py-[40px] md:px-[80px] md:py-[40px] sm:py-[20px] sm:px-[80px]
+          <div  className='lg:px-[80px] lg:py-[40px] md:px-[40px] md:py-[40px] sm:py-[20px] sm:px-[80px]
           md:px-[40px] md:py-[20px] px-[20px] py-[10px]' >
             <Slidermain/>
           </div>
@@ -42,7 +42,8 @@ const Home = () => {
           <div>
               <img src="images/index2.png" alt="logo" className='py-[20px] w-full opacity-0.1 h-[300px] lg:h-[500px]' />
           </div>
-          <div className="enquire-text font-normal text-white sm:px-[80px] lg:pt-[40px] pt-[10px] pb-[40px] text-center top-[25%] lg:top-[20%] lg:left-[15%]">
+          <div className="enquire-text font-normal text-white sm:px-[80px] lg:pt-[40px] pt-[10px] pb-[40px] text-center 
+          top-[25%] lg:top-[20%] lg:left-[15%]  md:top-[22%] md:left-[6%]">
             <p className='lg:text-[52px] sm:text-[36px] text-[22px] pb-[10px] '>For more information enquire now </p> 
             <p className='font-poppins lg:text-[18px] sm:text-[12px] text-[12px] px-[30px] pb-[10px] lg:pb-[20px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p> 
             <Popup /> {/* <button  className='text-[#000000] font-bold	lg:text-[14px] text-[8px] px-4 
